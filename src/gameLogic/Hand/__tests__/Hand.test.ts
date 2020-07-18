af2f3c5e29aa49673ea90619bcf456d61793931c
Pouya Oftadeh <poftadeh2@gmail.com>
2020-07-17 20:02:07 -0400

    test: enhance Hand tests to include value calculations

--- src/gameLogic/Hand/__tests__/Hand.test.ts
+++ src/gameLogic/Hand/__tests__/Hand.test.ts
@@ -2,10 +2,34 @@ import Hand from '..';
 import Card from '../../Card';
 
 describe('Hand', () => {
-  it('should construct a new hand', () => {
-    const firstCard = new Card('2', 'Spades');
-    const secondCard = new Card('King', 'Diamonds');
-    const hand = new Hand(firstCard, secondCard);
-    expect(hand.getCards()).toEqual([firstCard, secondCard]);
+  it('should calculate a hand of 21 value', () => {
+    const hand = new Hand();
+    hand.addCard(new Card('9', 'Hearts'));
+    hand.addCard(new Card('2', 'Spades'));
+    hand.addCard(new Card('Queen', 'Diamonds'));
+
+    expect(hand.getValue()).toBe(21);
+    expect(hand.getIsBlackJack()).toBe(false);
+    expect(hand.getIsBust()).toBe(false);
+  });
+
+  it('should calculate and detect a busted hand', () => {
+    const hand = new Hand();
+    hand.addCard(new Card('9', 'Clubs'));
+    hand.addCard(new Card('9', 'Clubs'));
+    hand.addCard(new Card('9', 'Clubs'));
+
+    expect(hand.getValue()).toBe(27);
+    expect(hand.getIsBust()).toBe(true);
+  });
+
+  it('should calculate and detect a blackjack hand', () => {
+    const hand = new Hand();
+    hand.addCard(new Card('King', 'Clubs'));
+    hand.addCard(new Card('Ace', 'Hearts'));
+
+    expect(hand.getValue()).toBe(21);
+    expect(hand.getIsBust()).toBe(false);
+    expect(hand.getIsBlackJack()).toBe(true);
   });
 });
