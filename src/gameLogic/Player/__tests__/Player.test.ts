a0441533ca3ebd19192966b677eef0b684edb08c
Pouya Oftadeh <poftadeh2@gmail.com>
2020-07-17 20:19:23 -0400

    feat: add player bet and win functionality with tests

--- src/gameLogic/Player/__tests__/Player.test.ts
+++ src/gameLogic/Player/__tests__/Player.test.ts
@@ -25,4 +25,18 @@ describe('Player', () => {
 
     expect(player.isHoldingBlackjack()).toBe(true);
   });
+
+  it('should apply a bet for the player', () => {
+    const player = new Player(3000);
+    player.bet(1000);
+    expect(player.getStack().getChips()).toBe(2000);
+    expect(player.getBetSize()).toBe(1000);
+  });
+
+  it('should apply a win multiplier', () => {
+    const player = new Player(50);
+    player.bet(50);
+    player.applyWinMultiplier(2);
+    expect(player.getStack().getChips()).toBe(100);
+  });
 });
--- src/gameLogic/Player/index.ts
+++ src/gameLogic/Player/index.ts
@@ -6,6 +6,8 @@ export default class Player {
 
   private stack: Stack;
 
+  private betSize: number | null = 0;
+
   constructor(startingChips: number) {
     this.stack = new Stack(startingChips);
     this.hand = new Hand();
@@ -37,5 +39,41 @@ export default class Player {
    */
   public isHoldingBlackjack(): boolean {
     return this.hand.getIsBlackJack();
+  }
+
+  /**
+   * Places a bet amount for the player.
+   * @param amount the amount of chips to wager.
+   */
+  public bet(amount: number): void {
+    const chipStackAmount = this.stack.getChips();
+    if (amount > chipStackAmount) {
+      throw new Error(
+        `Wager of ${amount} exceeds the $${chipStackAmount} chip stack.`,
+      );
+    }
+
+    this.stack.removeChips(amount);
+    this.betSize = amount;
+  }
+
+  /**
+   * Applies the win multiplier to the player's wager amount and adds it to the chip stack.
+   * @param multiplier the amount to multiply the bet amount by.
+   */
+  public applyWinMultiplier(multiplier: number): void {
+    if (this.betSize === null) {
+      throw new Error('Bet size is null');
+    }
+
+    this.stack.addChips(this.betSize * multiplier);
+    this.betSize = null;
+  }
+
+  /**
+   * Returns the player's bet size.
+   */
+  public getBetSize(): number {
+    return this.betSize ?? 0;
   }
 }
