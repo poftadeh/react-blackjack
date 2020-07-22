8814a9cc1d95f7b088d1338e52c988305b11bad0
Pouya Oftadeh <poftadeh2@gmail.com>
2020-07-22 00:24:28 -0400

    chore: add playerReducer to combineReducers

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import deckReducer from './deckReducer';
+import playerReducer from './playerReducer';
 
-export default combineReducers({ deckReducer });
+export default combineReducers({ deck: deckReducer, player: playerReducer });
