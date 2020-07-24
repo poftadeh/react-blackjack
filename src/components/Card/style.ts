7209663fc731bfec4e1892f16a8df0170da8e279
Pouya Oftadeh <poftadeh2@gmail.com>
2020-07-24 05:57:27 -0400

    feat: add animation to card on entry

--- src/components/Card/style.ts
+++ src/components/Card/style.ts
@@ -1,10 +1,22 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const CardContainer = styled.div`
   width: 8rem;
 `;
 
+const flipAnimation = keyframes`
+  from {
+    transform: translateX(3rem);
+  }
+
+  to {
+    transform: translateX(0);
+  }
+`;
+
 export const CardBase = styled.img`
+  position: relative;
   width: 100%;
   height: auto;
+  animation: ${flipAnimation} 0.5s ease-in-out;
 `;
