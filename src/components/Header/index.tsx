fabc161ce9c4fe70f817e40a80394fae5f5a82ca
Pouya Oftadeh <poftadeh2@gmail.com>
2020-07-23 23:39:53 -0400

    feat: add menu toggle button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { StyledHeader, Title } from './style';
+import { connect } from 'react-redux';
+import { StyledHeader, Title, MenuButton } from './style';
+import CombinedRootState from '../../types/CombinedRootState';
+import { setGameMenuVisibility } from '../../actions';
+import { GameAction } from '../../types/GameAction';
 
-const Header: React.FC = () => {
+interface Props {
+  isMenuVisible: boolean;
+  setGameMenuVisibility: (isGameMenuVisible: boolean) => GameAction;
+}
+
+const Header: React.FC<Props> = ({ setGameMenuVisibility, isMenuVisible }) => {
   return (
     <StyledHeader>
       <Title>React Blackjack</Title>
+      <MenuButton onClick={() => setGameMenuVisibility(!isMenuVisible)} />
     </StyledHeader>
   );
 };
 
-export default Header;
+const mapStateToProps = (state: CombinedRootState) => ({
+  isMenuVisible: state.game.isGameMenuVisible,
+});
+
+export default connect(mapStateToProps, { setGameMenuVisibility })(Header);
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -4,9 +4,22 @@ export const StyledHeader = styled.header`
   padding: 0.5rem 1rem;
   background-color: #2c3e50;
   text-transform: uppercase;
+  display: flex;
+  justify-content: space-between;
 `;
 
 export const Title = styled.h1`
   color: #fff;
   letter-spacing: 1.5px;
+`;
+
+export const MenuButton = styled.button`
+  border: none;
+  width: 3rem;
+  height: 3rem;
+  cursor: pointer;
+  background-color: inherit;
+  background-size: cover;
+  background-image: url(assets/images/menu.svg);
+  background-repeat: no-repeat;
 `;
